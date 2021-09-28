177ff818aa721dccca6f68fb46850ac65f572ebc
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-09-28 15:56:29 -0300

    Delete exercício 4.py

--- exercício 4.py
+++
@@ -1,5 +0,0 @@
-#Escreva um algoritmo que leia uma temperatura em graus Celsius e apresente a temperatura convertida em graus Fahrenheit. A fórmula de conversão é: F = (C * 1,8) + 32.
-
-celsius = float(input("Digite a temperatura em graus Celsius: "))
-f = (celsius * 1,8) + 32
-print(f"A temperatura é {f}F")
