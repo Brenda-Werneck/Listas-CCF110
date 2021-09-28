f3b23b8cf80ac90d302c61c2db00bd0634078703
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-09-28 15:54:39 -0300

    Delete exercício 11.py

--- exercício 11.py
+++
@@ -1,7 +0,0 @@
-#Escreva um algoritmo que leia um número e escreva caso ele seja maior que 20.
-
-num = float(input("Digite um número: "))
-if num > 20:
-    print(f"{num} é maior que 20")
-else:
-    print(f"{num} não é maior que 20")
