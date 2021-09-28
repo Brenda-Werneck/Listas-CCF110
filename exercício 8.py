fbc0f65dd31e746790d1d2b1a5664ad6322a7b78
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-09-28 15:55:08 -0300

    Delete exercício 8.py

--- exercício 8.py
+++
@@ -1,7 +0,0 @@
-#O custo ao consumidor de um carro novo é a soma do custo de fábrica com o percentual do distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que o percentual do distribuidor seja de 28% e os impostos de 45%, escreva um algoritmo que leia o custo de fábrica de um carro e escreva o custo do consumidor.
-
-custofab = float(input("Digite o custo de fábrica do carro em reais: "))
-distribuidor = custofab * 0,28
-impostos = custofab * 0,45
-custocons = custofab + distribuidor + impostos
-print(f"O custo do consumidor É R${custocons}")
