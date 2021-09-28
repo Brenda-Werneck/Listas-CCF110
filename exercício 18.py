d234ffdc0b55182725b45e0b8c8a17571172bca2
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-09-28 15:54:55 -0300

    Delete exercício 18.py

--- exercício 18.py
+++
@@ -1,8 +0,0 @@
-#Escreva um algoritmo para determinar se um número A é divisível por um outro número B. Esses valores devem ser fornecidos pelo usuário.
-
-A = int(input("Digite o primeiro número: "))
-B = int(input("Digite o segundo número: "))
-if A % B == 0:
-    print(f"{A} é divisível por {B}")
-else:
-    print(f"{A} não é divisível por {B}")
