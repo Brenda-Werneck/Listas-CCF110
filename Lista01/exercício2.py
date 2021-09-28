8a5df2dc06fdf675fa73f6bae7c3342646e3d1c8
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-09-28 15:52:40 -0300

    Delete exercício2.py

--- Lista01/exercício2.py
+++
@@ -1,6 +0,0 @@
-#Fazer um algoritmo que leia o valor do salário mínimo e o valor do salário de uma pessoa. Calcular e escrever quantos salários mínimos essa pessoa ganha.
-
-minimo = float(input("Digite o valor do salário mínimo atual em reais: "))
-salario = float(input("Digite o valor do seu salário atual em reais: "))
-qtdadesala = salario / minimo
-print(f"A quantidade de salários mínimos que você recebe é: {qtdadesala}")
