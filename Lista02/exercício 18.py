c880254ae60dca2381abf3fe6f0921cc5190707e
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-09-28 20:58:01 -0300

    Update exercício 18.py

--- Lista02/exercício 18.py
+++ Lista02/exercício 18.py
@@ -1,11 +1,11 @@
 #Escreva um algoritmo que leia um peso na Terra e o número de um planeta e escreva o valor do seu peso neste planeta. A relação de planetas é dada a seguir juntamente com o valor das gravidades relativas à Terra:
-##   Gravidade Relativa	Planeta
-#1	        0,37        Mercúrio
-#2	        0,88	    Vênus
-#3	        0,38	    Marte
-#4	        2,64	    Júpiter
-#5	        1,15	    Saturno
-#6	        1,17	    Urano
+##      Gravidade Relativa	    Planeta
+#1	            0,37            Mercúrio
+#2	            0,88	        Vênus
+#3	            0,38	        Marte
+#4	            2,64	        Júpiter
+#5	            1,15	        Saturno
+#6	            1,17	        Urano
 
 peso = float(input("Digite seu peso em kg:"))
 planetas = int(input("Digite o número do planeta escolhido: "))
