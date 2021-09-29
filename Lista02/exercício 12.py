cd1408504149d45930f7b55c3ef5a33e9c24b090
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-09-28 21:06:27 -0300

    Update exercício 12.py

--- Lista02/exercício 12.py
+++ Lista02/exercício 12.py
@@ -1,6 +1,5 @@
 #Escreva um algoritmo que leia um número e escreva a raiz quadrada do número caso ele seja positivo ou igual a zero e o quadrado do número caso ele seja negativo.
 
-
 num = int(input("Digite um número: "))
 if num >= 0:
     raiz = (num) ** (1/2)
