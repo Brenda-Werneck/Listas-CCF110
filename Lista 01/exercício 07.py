f23999929c4a5f682712e4ca5b5b51b0d01b87bd
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-01 08:08:40 -0300

    Create exercício 07.py

---
+++ Lista 01/exercício 07.py
@@ -0,0 +1,6 @@
+#Escreva um algoritmo que leia dois números reais e escreva o resultado da soma destes dois valores. Antes do resultado, deve ser escrito a seguinte mensagem “SOMA”.
+
+num = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
+soma = num + num2
+print(f"SOMA \n {soma}")
