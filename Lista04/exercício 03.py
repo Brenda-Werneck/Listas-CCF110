342492ac93e75cc759daf8352bf62730a32cfe11
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-02 20:17:52 -0300

    Delete exercício 03.py

--- Lista04/exercício 03.py
+++
@@ -1,8 +0,0 @@
-#Dada a razão de uma P.A. (progressão aritmética), e um termo qualquer k (), escreva um algoritmo para calcular qualquer outro termo, n, (). Leia os valores que considerar necessários.
-
-r = float(input("Digite a razão: "))
-ak = float(input("Digite o primeiro termo: "))
-n = float(input("Digite o termo desejado: "))
-k = float(input("Digite qualquer termo: "))
-an = ak + (n - k) * r
-print(an)
