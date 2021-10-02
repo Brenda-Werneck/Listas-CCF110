55d933f299afa8711266224fc214a5a1c07ee435
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-02 20:18:00 -0300

    Delete exercício 05.py

--- Lista04/exercício 05.py
+++
@@ -1,5 +0,0 @@
-#Considere que o número de uma placa de veículo é composto por quatro algarismos. Construa um algoritmo que leia este número e apresente o algarismo correspondente à casa das dezenas.
-
-placa = int(input("Digite um número: "))
-dezenas = (placa % 100) // 10
-print(dezenas)
