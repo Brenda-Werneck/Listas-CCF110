28c87ea7f0be2e70a56d07dc9fc7775d9677a6f6
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-02 20:18:43 -0300

    Delete exercício 09.py

--- Lista06/exercício 09.py
+++
@@ -1,8 +0,0 @@
-#Elabore um algoritmo que leia os valores e realize a soma de cada um dos elementos de dois vetores de 5 posições e armazene o resultado em um terceiro vetor, que deve ter seus elementos apresentados.
-
-vetor1 = [int(input(f"Digite {x + 1}ª número do vetor 1: ")) for x in range(5)]
-vetor2 = [int(input(f"Digite {x + 1}ª número do vetor 2: ")) for x in range(5)]
-vet_soma = [vetor1[x] + vetor2[x] for x in range(5)]
-
-for i in range(5):
-    print(f"{vetor1[i]} + {vetor2[i]} = {vet_soma[i]}")
