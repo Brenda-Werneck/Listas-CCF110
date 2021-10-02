2f516183dd370df241cca73f78ae8a17d8990ca6
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-02 20:16:29 -0300

    Delete exercício 04.py

--- Lista02/exercício 04.py
+++
@@ -1,8 +0,0 @@
-#Faça um algoritmo que leia a idade de uma pessoa expressa em dias e mostre-a expressa em anos, meses e dias.
-
-dias = int(input("Digite a sua idade em dias: "))
-anos = dias // 365
-resto = dias % 365
-meses = resto // 30
-dias = resto % 30
-print(f"Você viveu {anos} anos, {meses} meses e {dias} dias")
