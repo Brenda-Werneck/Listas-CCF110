073790e431f08f012578c79209fc4a508d8470bd
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-02 20:13:04 -0300

    Delete exercício 09.py

--- Lista01/exercício 09.py
+++
@@ -1,7 +0,0 @@
-#Elaborar um algoritmo que efetue a leitura de três números inteiros e apresente a soma dos seus quadrados.
-
-num = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
-num3 = int(input("Digite o terceiro número: "))
-somaqua = (num ** 2) + (num2 ** 2)  (num3 ** 2)
-print(f"A soma dos quadrados de {num},{num2} e {num3} é {somaqua}")
