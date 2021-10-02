4449eecde2adfb52942100051738756727f1bd1d
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-02 20:12:54 -0300

    Delete exercício 06.py

--- Lista01/exercício 06.py
+++
@@ -1,6 +0,0 @@
-#Escreva um algoritmo que leia um número inteiro e escreva o seu sucessor e seu antecessor.
-
-num = int(input("Digite um número inteiro: "))
-antecessor = num - 1
-sucessor = num + 1
-print(f"O antecessor de {num} é {antecessor} \nO sucessorr de {num} é {sucessor}")
