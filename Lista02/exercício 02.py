b2dfac46dfef73bdc308b4eb16c41a6fbf99a536
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-02 20:16:25 -0300

    Delete exercício 02.py

--- Lista02/exercício 02.py
+++
@@ -1,7 +0,0 @@
-#Construa um algoritmo que leia dois valores numéricos e efetue a adição; caso o resultado seja maior que 10, apresentá-lo.
-
-num = float(input("Digite um número: "))
-num2 = float(input("Digite outro número: "))
-soma = num + num2
-if soma > 10:
-    print(soma)
