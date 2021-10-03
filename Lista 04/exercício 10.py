b01a3fae21293a6c4ea8334bfd5eaa0526ac8e3a
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-03 00:04:31 -0300

    Create exercício 10.py

---
+++ Lista 04/exercício 10.py
@@ -0,0 +1,6 @@
+#Criar um algoritmo que efetue o cálculo do salário líquido de um professor. Os dados fornecidos serão: valor da hora aula, número de aulas dadas no mês e percentual de desconto do INSS.
+
+horas = int(input("Digite as horas: "))
+minutos = int(input("Digite os minutos: "))
+totmin = horas * 60 + minutos
+print(f"Passaram-se {totmin} minutos desde 00h00min")
