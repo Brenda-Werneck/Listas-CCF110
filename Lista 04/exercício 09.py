0a62a4e41c447a50494a722b12481c53bb225f8f
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-03 00:04:29 -0300

    Create exercício 09.py

---
+++ Lista 04/exercício 09.py
@@ -0,0 +1,5 @@
+#Em épocas de pouco dinheiro, os comerciantes estão procurando aumentar suas vendas oferecendo descontos. Faça um algoritmo que possa entrar com o valor de um produto e escreva o novo valor tendo em vista que o desconto foi de 9%. Além disso, escreva o valor do desconto.
+
+prod = int(input("Digite o valor do produto em reais: "))
+desconto = prod * 0,91
+print(f"Produto com desconto de 9% = {desconto}")
