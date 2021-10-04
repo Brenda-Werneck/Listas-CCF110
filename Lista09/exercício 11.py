840eacb794d708dba7107098eb765d81ac0d2fe0
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-04 10:44:55 -0300

    Delete exercício 11.py

--- Lista09/exercício 11.py
+++
@@ -1,8 +0,0 @@
-#Crie um algoritmo que entre com valores inteiros para uma matriz M[3x3] e escreva a matriz final, conforme mostrado a seguir: A matriz gira 270º
- 
-M = [[int(input(f"Valor para({i + 1},{j + 1}): ")) for j in range(3)] for i in range(3)]
-M270 = [[M[j][2 - i] for j in range(3)] for i in range(3)]
-print("Matriz após girar 180ª")
-for i in range(3):
-    print(M270[i])
-
