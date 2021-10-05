673de8f82b8fed3ac54201e55bb4f5b306f6ac74
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-05 10:28:59 -0300

    Delete exercício 10.py

--- Lista10/exercício 10.py
+++
@@ -1,7 +0,0 @@
-#Faça um programa que lê uma string que representa uma cadeia de DNA e gera a cadeia complementar. Exemplo:
-#	Entrada: AATCTGCAC
-#	Saída: TTAGACGTG
-
-n, m, nn = input(), {"A": "T", "T": "A", "G": "C", "C": "G", "A": "T"}, ""
-for x in n: nn += m[x]
-print(nn)
