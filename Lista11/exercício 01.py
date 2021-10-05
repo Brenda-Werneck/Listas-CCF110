2812467406f97a0df2b809ae607e6a5abc6a0771
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-05 10:39:55 -0300

    Delete exercício 01.py

--- Lista11/exercício 01.py
+++
@@ -1,7 +0,0 @@
-#Faça um programa que escreve um arquivo com números em ordem decrescente de 200 a 50. Cada número deve estar em uma linha. Dê o nome que quiser para o arquivo.
-
-arquivo1 = open("decrescente.txt", "w")
-
-for i in range(200, 49, -1):
-    arquivo1.write(i + "\n")
-arquivo1.close()
