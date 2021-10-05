1923e532cd038731b2feb4b1476605cde999ad38
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-05 10:29:01 -0300

    Delete exercício 11.py

--- Lista10/exercício 11.py
+++
@@ -1,8 +0,0 @@
-#Faça um programa que leia uma frase e retorne o número de palavras que a frase contém. 
-
-text = input()
-substring = " "
-
-total_occurrences = (text.count(substring) + 1)
-
-print("A frase tem " + str(total_occurrences) + " palavras")
