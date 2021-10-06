616b23fc65a66c78ebf044298fa7e361a59be1b3
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-06 19:29:59 -0300

    Update exercício 10.c

---
+++ Lista 12/exercício 10.c
@@ -0,0 +1,34 @@
+//Escreva um algoritmo que leia um vetor de tamanho n (informado pelo usuário) e escreva a soma de todos os elementos de índice par.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+main()
+{
+    int n, soma, i;
+    
+    printf("\nDigite o tamanho do seu vetor: \n");
+    scanf("%d", &n);
+    int vetor[n];
+    soma = 0;
+
+    for (i=0; i<n; i++)
+    {
+        printf("\nDigite um valor para o vetor: \n");
+        scanf("%d", &vetor[i]);
+    }
+
+    for (i=0; i<n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            soma += vetor[i];
+        }
+    }
+        
+    printf("\nSoma = %d", soma);
+
+    return 0;
+}
