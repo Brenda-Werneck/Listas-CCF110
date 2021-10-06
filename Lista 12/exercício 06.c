569afcb556abb89a59860bee19422b1f457578ae
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-06 11:00:41 -0300

    Create exercício 06.c

---
+++ Lista 12/exercício 06.c
@@ -0,0 +1,23 @@
+//Fazer um algoritmo para calcular e escrever a seguinte soma:
+//S = (37x38)/1 + (36x37)/2 + (35x36)/3 + ...+(1x2)/37
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+main()
+{
+    float soma, cont, i;
+
+    soma = 0;
+    cont = 1;
+
+    for (i=37; i>0; i--)
+    {
+        soma += i * (i + 1) / cont;
+        cont += 1;
+    }
+        printf("\nSoma = %f\n", soma);
+
+    return 0;
+}
