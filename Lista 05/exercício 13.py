3d8b8a68e2a2aa432b79cc524e04f21e021cb91f
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 17:58:19 -0300

    Update exercício 13.py

---
+++ Lista 05/exercício 13.py
@@ -0,0 +1,1 @@
+#Fazer um algoritmo que calcule e escreva o número de grãos de milho que se pode colocar num tabuleiro de xadrez, colocando 1 no primeiro quadro e nos quadros seguintes o dobro do quadro anterior. São 64 quadros e a fórmula é:
