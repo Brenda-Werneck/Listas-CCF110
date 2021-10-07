1d39c58ca1c8fa5884bbcedf3581baa556c7e160
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 16:19:16 -0300

    Update exercício 04.c

---
+++ Lista 13/exercício 04.c
@@ -0,0 +1,4 @@
+//Elabore um algoritmo que inverta um número de 3 dígitos. O algoritmo deve utilizar uma função que receba um número e o retorne invertido.
+//Exemplo:
+//Número digitado: 154
+//Número invertido: 451
