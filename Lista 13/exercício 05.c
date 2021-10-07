bff0dbce975e911a6f3f3d77bba6d1479af2e503
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 16:19:20 -0300

    Update exercício 05.c

---
+++ Lista 13/exercício 05.c
@@ -0,0 +1,8 @@
+//Elabore um algoritmo que faça, com funções, as seguintes conversões de temperatura:
+//Fahrenheit para Celsius;
+//Fahrenheit para Kelvin;
+//Celsius para Fahrenheit;
+//Celsius para Kelvin;
+//Kelvin para Fahrenheit;
+//Kelvin para Celsius;
+//Obs.: Desenvolva uma função para cada conversão.
