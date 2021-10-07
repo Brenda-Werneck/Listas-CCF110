e9fd0ee03a7832d7bad7af2f2a4c911a734e5bed
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-06 22:05:13 -0300

    Update exercício 14.c

--- Lista 12/exercício 14.c
+++ Lista 12/exercício 14.c
@@ -1,1 +1,38 @@
- 
+//Leia valores inteiros para a matriz A[3x5]. Gere e escreva o vetor SL (soma das 3 linhas), onde cada elemento é a soma dos elementos de uma linha da matriz A. Faça o trecho que gera a matriz SL separado (laços de repetição) da entrada e da saída de dados.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+main()
+{
+    int A[3][5], vetorSL[3];
+    int i, j, posicaoSl;
+
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j <3; i++)
+        {
+            printf("Digite um valor: ");
+            scanf("%d", &A[i][j]);
+        }
+    }    
+    posicaoSl = 0;
+
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j <3; i++)
+        {
+            vetorSL[posicaoSl] += A[i][j];
+        }
+        posicaoSl += 1;
+    }
+
+    for (i = 0; i <3; i++)
+    {
+        printf("%d", vetorSL[i]);
+    }
+
+    system("PAUSE");
+    return 0;
+}
