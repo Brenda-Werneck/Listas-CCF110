0feef55bfd626bd17d503e315d326274e83b195e
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 17:58:23 -0300

    Update exercício 14.py

---
+++ Lista 05/exercício 14.py
@@ -0,0 +1,4 @@
+#A série de FETUCCINE é gerada da seguinte forma: os dois primeiros termos são fornecidos pelo usuário; a partir daí, os termos são gerados com a soma ou subtração dos dois termos anteriores, ou seja:
+# + para i ímpar
+# - para i par
+#Criar um algoritmo que escreva os N primeiros termos da série de FETUCCINE, sabendo-se que para existir esta série serão necessários pelo menos três termos.
