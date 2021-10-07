bc8b5084feeb94242c9cc22d844895ad0d9b6aa7
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 16:19:30 -0300

    Update exercício 07.c

---
+++ Lista 13/exercício 07.c
@@ -0,0 +1,2 @@
+//Escreva um algoritmo que leia 5 notas de 10 alunos. Elabore uma função que some as notas de cada aluno e retorne se o aluno foi aprovado ou reprovado.
+//Obs.: Se a média das somas das notas for menor que 60, o aluno estará reprovado.
