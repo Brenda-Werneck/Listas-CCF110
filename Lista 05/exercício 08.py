0e86a70103910d7ec366ab341d57373e56db0b60
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 17:57:54 -0300

    Update exercício 08.py

---
+++ Lista 05/exercício 08.py
@@ -0,0 +1,1 @@
+#Construa um algoritmo para encontrar o maior e o menor número de uma série de números positivos fornecidos pelo usuário através do teclado. O dado finalizador é o número –1, e este não deve ser considerado.
