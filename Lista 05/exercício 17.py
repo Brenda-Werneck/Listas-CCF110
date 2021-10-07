f0ee43cdfb2432c2e434d956d5527ed72e2e2508
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 17:58:39 -0300

    Update exercício 17.py

---
+++ Lista 05/exercício 17.py
@@ -0,0 +1,6 @@
+#Criar um algoritmo que possa ler um conjunto de pedidos de compra e calcule o valor total da compra. Cada pedido é composto pelos seguintes campos:
+#	Número de pedido;
+#	Data do pedido (dia, mês, ano);
+#	Preço unitário;
+#	Quantidade.
+#O algoritmo deverá processar novos pedidos até que o usuário digite (zero) como número de pedido.
