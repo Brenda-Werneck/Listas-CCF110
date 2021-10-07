1769540cfc5d8a14da472d259fa3248451cf5db1
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 17:58:31 -0300

    Update exercício 15.py

---
+++ Lista 05/exercício 15.py
@@ -0,0 +1,16 @@
+#Um cinema possui capacidade de 100 lugares e está sempre com ocupação total. Certo dia, cada espectador respondeu a um questionário, no qual constava:
+#	idade;
+#	opinião em relação ao filme, segundo as seguintes notas:
+#Nota	    Significado
+#A	        Ótimo
+#B	        Bom
+#C	        Regular
+#D	        Ruim
+#E	        Péssimo
+
+#Elabore um algoritmo que, lendo estes dados, calcule e escreva:
+#	a quantidade de respostas ótimo;
+#	a diferença percentual entre respostas bom e regular;
+#	a média de idade das pessoas que responderam ruim;
+#	a porcentagem de respostas péssimo e a maior idade que utilizou esta opção;
+#	a diferença de idade entre a maior idade que respondeu ótimo e a maior idade que respondeu ruim.
