3eff5322c2ac04aa2daae307918f92c89a7d9ba0
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-08 15:03:35 -0300

    Update exercício 11.py

--- Lista 05/exercício 11.py
+++ Lista 05/exercício 11.py
@@ -1,1 +1,8 @@
 #Escreva um algoritmo que calcule o resto da divisão de A por B (número inteiros e positivos), ou seja, A mod B, através de subtrações sucessivas. Esses dois valores são passados pelo usuário através do teclado.
+
+A = int(input("Digite um número inteiro: "))
+B = int(input("Digite outro número inteiro: "))
+
+while A >= B:
+    A = int(A - B)
+print(f"Resto da divisão = {A}")
