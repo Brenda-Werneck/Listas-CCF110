58ac82efc67d28b7b6edd14006ff51a4a30f5d82
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-08 16:15:49 -0300

    Update exercício 16.py

--- Lista 05/exercício 16.py
+++ Lista 05/exercício 16.py
@@ -3,3 +3,29 @@
 #	A atleta do sexo feminino mais pesada;
 #	A média de idade dos atletas.
 #Deverão ser lidos dados dos atletas até que seja digitado o nome @ para um atleta.
+
+maiorAlt = 0
+maiorPeso = 0
+cont = 0
+somaidade = 0
+
+while True:
+    nome = input("Digite o nome: ")
+    if nome == "@":
+        break
+    sexo = input("Digite o sexo (M ou F): ")
+    idade = int(input("Digite a idade: "))
+    peso = float(input("Digite o peso em kg: "))
+    alt = float(input("Digite a altura em metros: "))
+    if sexo == "M" and alt > maiorAlt:
+        maiorAlt = alt
+        altnome = nome
+    elif sexo == "F" and peso > maiorPeso:
+        maiorPeso = peso
+        pesonome = nome
+    somaidade += idade
+    cont += 1
+
+print(f"O atleta mais alto é o: {altnome} com {maiorAlt}m")
+print(f"A atleta mais pesada é a: {pesonome} com {maiorPeso}kg")
+print(f"A média das idades é {somaidade / cont} anos")
