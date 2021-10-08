3c21e909aca206c287ff9c0e6c9dfeb5d50e9e55
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-08 16:39:13 -0300

    Update exercício 18.py

--- Lista 05/exercício 18.py
+++ Lista 05/exercício 18.py
@@ -1,1 +1,11 @@
 #Chico tem 1,50m e cresce 2 centímetros por ano, enquanto Juca tem 1,10m e cresce 3 centímetros por ano. Construir um algoritmo que calcule iterativamente e escreva quantos anos serão necessários para que Juca seja maior que Chico.
+
+cont = 0
+chico = 150
+juca = 110
+
+while juca <= chico:
+    chico += 2
+    juca += 3
+    cont += 1
+print(f"Serão necessários {cont} anos")
