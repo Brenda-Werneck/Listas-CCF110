027b8bef6dcf86d4055831a454d95565e0c55270
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-08 16:39:07 -0300

    Update exercício 17.py

--- Lista 05/exercício 17.py
+++ Lista 05/exercício 17.py
@@ -4,3 +4,15 @@
 #	Preço unitário;
 #	Quantidade.
 #O algoritmo deverá processar novos pedidos até que o usuário digite (zero) como número de pedido.
+
+total = 0
+
+while True:
+    num = int(input("Digite o número do pedido: "))
+    if num == 0:
+        break
+    data = input("Data do pedido DD/MM/AA: ")
+    precouni = float(input("Digite o preço unitário: "))
+    quant = int(input("Digite a quantidade: "))
+    total += precouni * quant
+print(f"Total = R${total}")
