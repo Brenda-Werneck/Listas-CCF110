3edb7dd274f11b43ee8a4a2b9874814acb37fa4a
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 21:34:02 -0300

    Update exercício 08.py

--- Lista 05/exercício 08.py
+++ Lista 05/exercício 08.py
@@ -1,1 +1,15 @@
 #Construa um algoritmo para encontrar o maior e o menor número de uma série de números positivos fornecidos pelo usuário através do teclado. O dado finalizador é o número –1, e este não deve ser considerado.
+
+n = 0
+maior = 0
+menor = 10000
+
+while True:
+    n = int(input("Digite um número: "))
+    if n <= 0:
+        break
+    if n > maior:
+        maior = n
+    if n < menor:
+        menor = n
+print(f"Maior = {maior} \nMenor = {menor}")
