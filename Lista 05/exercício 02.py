d8d2001b9fbfde648124c53614b402a4a6195df4
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 21:14:39 -0300

    Update exercício 02.py

--- Lista 05/exercício 02.py
+++ Lista 05/exercício 02.py
@@ -4,3 +4,13 @@
 #De 501 a 1000	        30% do valor do saldo médio
 #De 1001 a 3000	        40% do valor do saldo médio
 #Acima de 3001	        50% do valor do saldo médio
+
+saldomedio = int(input("Digite o saldo médio: "))
+if saldomedio < 500:
+    print(f"Com saldo médio de R${saldomedio}: nenhum crédito")
+if saldomedio > 500 and saldomedio <= 1000:
+    print(f"Com saldo médio de {saldomedio}: crédito de 30% = R${0.3 * saldomedio}")
+if saldomedio > 1001 and saldomedio <= 3000:
+    print(f"Com saldo médio de {saldomedio}: crédito de 40% = R${0.4 * saldomedio}")
+if saldomedio > 3001:
+    print(f"Com saldo médio de {saldomedio}: crédito de 50% = R${0.5 * saldomedio}")
