943f4ff4f66e3762ca994b20196522ee5da4161e
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 21:14:53 -0300

    Update exercício 04.py

--- Lista 05/exercício 04.py
+++ Lista 05/exercício 04.py
@@ -1,1 +1,8 @@
 #Criar um algoritmo que escreva todos os números de 1 até 100, inclusive, e a soma do quadrado desses números.
+
+somaquad = 0
+
+for i in range(101):
+    somaquad += ((i + 1) ** 2)
+    print(i + 1)
+print(somaquad)
