af91b723ac7fd80a901f13fbdc5315eaffcbc39b
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-08 16:06:01 -0300

    Update exercício 14.py

--- Lista 05/exercício 14.py
+++ Lista 05/exercício 14.py
@@ -2,3 +2,21 @@
 # + para i ímpar
 # - para i par
 #Criar um algoritmo que escreva os N primeiros termos da série de FETUCCINE, sabendo-se que para existir esta série serão necessários pelo menos três termos.
+
+A1 = int(input("Digite o primeiro termo: "))
+A2 = int(input("Digite o segundo termo: "))
+N = int(input("Digite o número de termos: "))
+
+if N < 3:
+    print('Número de termos insuficiente para formar uma sequência.')
+    exit()
+
+for i in range(N):
+    i += 3
+    if i % 2 == 0:
+        A3 = A2 - A1
+    else:
+        A3 = A2 + A1
+    print(A1, end=' ')
+    A1 = A2
+    A2 = A3
