60ba8364d0256db1ba41bd583b68daba35279541
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 22:14:18 -0300

    Update exercício 10.py

--- Lista 05/exercício 10.py
+++ Lista 05/exercício 10.py
@@ -1,1 +1,9 @@
 #Escreva um algoritmo que realize o produto de A (número real) por B (número inteiro), ou seja, A * B, através de adições (somas). Esses dois valores são passados pelo usuário através do teclado.
+
+A = float(input("Digite o número real: "))
+B = int(input("Digite o número inteiro: "))
+soma = 0
+
+for i in range(B):
+    soma += A
+print(f"Produto de {A} x {B} = {soma}")
