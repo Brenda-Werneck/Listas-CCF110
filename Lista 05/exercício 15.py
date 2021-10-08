118f3cd52ccb0d688dabe92c1d9747c4e37d6577
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 21:15:15 -0300

    Update exercício 15.py

--- Lista 05/exercício 15.py
+++ Lista 05/exercício 15.py
@@ -14,3 +14,45 @@
 #	a média de idade das pessoas que responderam ruim;
 #	a porcentagem de respostas péssimo e a maior idade que utilizou esta opção;
 #	a diferença de idade entre a maior idade que respondeu ótimo e a maior idade que respondeu ruim.
+
+contA = 0 
+contB = 0
+contC = 0 
+contD = 0
+contE = 0 
+somaidade = 0
+maioridaderuim = 0
+maioridadeotimo = 0
+maioridadepessimo = 0 
+matriz = [[0 for i in range(100)] for j in range(2)]
+
+for j in range(100):
+    matriz[0][j] = int(input("Digite a sua idade: "))
+    matriz[1][j] = input("Digite sua nota ao filme: ")
+for j in range(100):
+    if matriz[1][j] == "A":
+        contA += 1
+        if matriz[0][j] > maioridadeotimo:
+            maioridadeotimo = matriz[0][j]
+    if matriz[1][j] == "B":
+        contB += 1
+    if matriz[1][j] == "C":
+        contC += 1
+    if matriz[1][j] == "D":
+        somaidade += matriz[0][j]
+        contD += 1
+        if maioridaderuim > matriz[0][j]:
+            maioridaderuim = matriz[0][j]
+    if matriz[1][j] == "E":
+        contE += 1
+        if matriz[0][j] > maioridadepessimo:
+            maioridadepessimo = matriz[0][j]
+difidadeAeD = maioridadeotimo - maioridaderuim
+difperc = contB - contC
+mediaidaderuim = somaidade / contD
+print(f"Quantidade de respostas ótimo: {contA}")
+print(f"Diferença percentual entre respostas bom e regular: {difperc}%")
+print(f"Média da idade das pessoas que responderam ruim: {mediaidaderuim} anos")
+print(f"Porcentagem de respostas péssimo: {contE}%")
+print(f"Maior idade que respondeu péssimo: {maioridadepessimo} anos")
+print(f"Diferença de idade entre a maior idade que respondeu ótimo e a maior idade que respondeu ruim: {difidadeAeD} anos")
