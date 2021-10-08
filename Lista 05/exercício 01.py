eb53f5bcca0d04b414840c2e57d36884cec6c4b3
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 21:14:34 -0300

    Update exercício 01.py

--- Lista 05/exercício 01.py
+++ Lista 05/exercício 01.py
@@ -1,4 +1,6 @@
 #Construa um algoritmo para determinar se o indivíduo está com um peso favorável. Essa situação é determinada através do IMC (Índice de Massa Corporal), que é definida como sendo a relação entre o peso (PESO) e o quadrado da Altura (ALTURA) do indivíduo. Ou seja,
+
+#   IMC = PESO / (ALTURA^2)
  
 #A situação do peso é determinada pela tabela abaixo:
 #Condição	            Situação
@@ -7,3 +9,17 @@
 #IMC de 25 até 30	    Sobrepeso
 #IMC de 30 até 40	    Obeso
 #IMC de 40 e acima	    Obeso Mórbido
+
+altura = float(input("Digite a sua altura em metros: "))
+peso = float(input("Digite o seu peso em kg: "))
+IMC = peso / (altura ** 2)
+if IMC < 20:
+    print("Situação: abaixo do peso")
+if IMC >= 20 and IMC < 25:
+    print("Situação: peso normal")
+if IMC >= 25 and IMC < 30:
+    print("Situação: sobrepeso")
+if IMC >= 30 and IMC < 40:
+    print("Situação: obeso")
+if IMC >= 40:
+    print("Situação: obeso mórbido")
