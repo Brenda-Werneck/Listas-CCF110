662467152aa77e8bab97ac87a3df8c5ab5e41717
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 21:15:04 -0300

    Update exercício 06.py

--- Lista 05/exercício 06.py
+++ Lista 05/exercício 06.py
@@ -1,1 +1,11 @@
 #Escreva um algoritmo que receba 15 números e escreva quantos números maiores que 30 foram digitados.
+
+cont = 0
+vetor = []
+for i in range(15):
+    num = int(input(f"Digite o {i+1}º número: "))
+    vetor.append(num)
+for i in range(15):
+    if vetor[i] > 30:
+        cont += 1
+print(cont)
