86dde6f9993b6d5f84e104ce6682528b5b4fe3d0
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-08 15:52:57 -0300

    Update exercício 13.py

--- Lista 05/exercício 13.py
+++ Lista 05/exercício 13.py
@@ -1,1 +1,6 @@
 #Fazer um algoritmo que calcule e escreva o número de grãos de milho que se pode colocar num tabuleiro de xadrez, colocando 1 no primeiro quadro e nos quadros seguintes o dobro do quadro anterior. São 64 quadros e a fórmula é:
+
+soma = 0
+for i in range(64):
+    soma += (2 ** i)
+print(f"{soma} grãos de milho")
