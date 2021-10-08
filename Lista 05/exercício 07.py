bb811613ca5dd1460f8921feeaeb807107165892
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 21:15:10 -0300

    Update exercício 07.py

--- Lista 05/exercício 07.py
+++ Lista 05/exercício 07.py
@@ -1,1 +1,15 @@
 #Criar um algoritmo que receba vários números inteiros e positivos e escreva a média dos números múltiplos de 3. A execução deve encerrar quando um número não positivo for lido.
+
+n = 0 
+soma = 0
+cont = 0
+while True:
+    n = int(input("Digite um número: "))
+    if n <= 0:
+        break
+    if n % 3 == 0:
+        soma += n
+        cont += 1
+    media = soma / cont
+print(media)
+    
