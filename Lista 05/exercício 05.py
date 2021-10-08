e7fde8d4eddacacc44edc808ad420771b527e32c
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-07 21:14:58 -0300

    Update exercício 05.py

--- Lista 05/exercício 05.py
+++ Lista 05/exercício 05.py
@@ -1,1 +1,7 @@
 #Criar um algoritmo que leia um número (NUM) e então escreva os múltiplos de 3 e 5, ao mesmo tempo, no intervalo fechado de 1 a NUM.
+
+NUM = int(input("Digite o fim do intervalo: "))
+
+for i in range(NUM + 1):
+    if (i + 1) % 15:
+        print(i + 1)
