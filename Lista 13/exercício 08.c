45cc11b4919e76011421470eabc2b58ff2d91c26
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-10 00:46:04 -0300

    Update exercício 08.c

--- Lista 13/exercício 08.c
+++ Lista 13/exercício 08.c
@@ -1,1 +1,26 @@
 //Crie uma função que leia uma medida em metros e converta-a para pés e polegadas, sabendo que 1 pé = 12 polegadas e 1 metro = 39.37 polegadas.
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double polegadas(double m)
+{
+    return m * 39.37;
+}
+
+double pes(double polegadas)
+{
+    return polegadas / 12;
+}
+
+int main()
+{
+    double metro;
+    printf("Digite os metros: ");
+    scanf("%lf", metro);
+    printf("\nPolegadas = %lf", polegadas(metro));
+    printf("\nPés = %lf\n", pes(polegadas(metro)));
+    system("PAUSE");
+    return 0;
+}
