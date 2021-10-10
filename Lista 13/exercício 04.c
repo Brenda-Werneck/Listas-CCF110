e8540694a639cc76d7b41d16c0ab33bfe2f01f6c
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-09 22:54:26 -0300

    Update exercício 04.c

--- Lista 13/exercício 04.c
+++ Lista 13/exercício 04.c
@@ -2,3 +2,24 @@
 //Exemplo:
 //Número digitado: 154
 //Número invertido: 451
+
+#include <stdio.h>
+
+int inver(int n)
+{
+    int u, d, c, mun;
+    u = (n % 100) % 10;
+    d = (n % 100) / 10;
+    c = (n - n % 100) / 100;
+    mun = u * 100+ d * 10 + c;
+    return(mun);
+}
+int main()
+{
+    int num,res;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+    printf("Numero digitado: %d\n", num);
+    printf("Numero invertido: %d\n", inver(num));
+    return(0);
+}
