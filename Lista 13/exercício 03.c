75fd6230c1b20b10ba41157751b339ff8169b652
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-09 23:21:11 -0300

    Update exercício 03.c

--- Lista 13/exercício 03.c
+++ Lista 13/exercício 03.c
@@ -1,2 +1,48 @@
 //Crie um algoritmo que utilize uma função que define se um número é primo ou não, a função retornará 1 ou 0 para informar tal resultado.
 
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int teste(int n)
+{
+    return n;
+}
+
+void PRIMO(int n)
+{
+    int i, cont;
+    int primo = teste(n);
+    cont = 0;
+    for (i = 2; i < primo; i++)
+    {
+        if (primo % i == 0)
+        {
+            cont += 1;
+        }
+        else
+        {
+            cont = cont;
+        }
+    }
+    if (cont != 0)
+    {
+        printf("0\n");
+    }
+    else
+    {
+        printf("1\n");
+    }
+}
+
+int main()
+{
+    int n;
+    n = 0;
+    printf("\nDigite um número: ");
+    scanf("%d", &n);
+    PRIMO(n);
+
+    system("PAUSE");
+    return 0;
+}
