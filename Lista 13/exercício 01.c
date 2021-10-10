c9bbbc991064badcc9838d0cda074c7b4efb3fcf
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-09 22:54:14 -0300

    Update exercício 01.c

--- Lista 13/exercício 01.c
+++ Lista 13/exercício 01.c
@@ -2,3 +2,45 @@
 //O valor de delta;
 //O valor das raízes.
 
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double Delta(double a, double b, double c)
+{
+    return (b * b) - 4 * a * c;
+}
+
+void raizes(double a, double b, double c)
+{
+    double delta = Delta(a, b, c);
+    if (delta < 0)
+    {
+        printf("Delta negativo\n");
+    }
+    else
+    {
+        double x1 = (-b + sqrt(delta)) / (2 * a);
+        double x2 = (-b - sqrt(delta)) / (2 * a);
+        printf("x1 = %lf\n", x1);
+        printf("x2 = %lf\n", x2);
+    }
+}
+
+int main()
+{
+    double a, b, c;
+    a = 0;
+    b = 0;
+    c = 0;
+    printf("\nDigite o valor de a: ");
+    scanf("%lf", &a);
+    printf("\nDigite o valor de b: ");
+    scanf("%lf", &b);
+    printf("\nDigite o valor de c: ");
+    scanf("%lf", &c);
+    raizes(a, b, c);
+
+    system("PAUSE");
+    return 0;
+}
