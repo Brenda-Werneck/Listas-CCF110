2d94815880c100fd1e7c47cb22f5d9e0feba81e1
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-10 00:17:59 -0300

    Update exercício 06.c

--- Lista 13/exercício 06.c
+++ Lista 13/exercício 06.c
@@ -1,1 +1,26 @@
 //Escreva um algoritmo para ler três notas de um aluno. Elabore uma função para calcular e retornar a média aritmética destas notas.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+double Media(double n1, double n2, double n3)
+{
+    return (n1 + n2 + n3) / 3;
+}
+
+int main()
+{
+    int n1, n2, n3;
+
+    printf("Digite a primeira nota: ");
+    scanf("%d", &n1);
+    printf("Digite a segunda nota: ");
+    scanf("%d", &n2);
+    printf("Digite a terceira nota: ");
+    scanf("%d", &n3);
+
+    printf("Média = %.2lf\n", Media(n1, n2, n3));
+
+    system("PAUSE");
+    return 0;
+}
