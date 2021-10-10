3202c6e4d4dfd23f0cfa60340d790b9d00af88c7
Angelo <62099585+acmachado14@users.noreply.github.com>
2021-10-10 12:33:29 -0300

    Update exercício 14.c
    Mudança no for

--- Lista 12/exercício 14.c
+++ Lista 12/exercício 14.c
@@ -11,7 +11,7 @@ main()
 
     for (i = 0; i < 5; i++)
     {
-        for (j = 0; j <3; i++)
+        for (j = 0; j <3; j++)
         {
             printf("Digite um valor: ");
             scanf("%d", &A[i][j]);
