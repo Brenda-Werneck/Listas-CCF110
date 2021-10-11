f4afa646f302c5024e93e70d94eef9c06bd8a17f
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-11 20:39:56 -0300

    Update exercício 19.c

---
+++ Lista 13/exercício 19.c
@@ -0,0 +1,26 @@
+//Elabore um algoritmo que receba os valores de um vetor de inteiros de 15 posições. Crie uma função que calcule e retorne a soma destes valores.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int soma(int vetor[15])
+{
+    int soma, i, j;
+    soma = 0;
+    for (i = 0; i < 15; i++)
+    {
+        soma += vetor[i];
+    }
+    return(soma);
+}
+
+int main()
+{
+    int vetor[15], i, j;
+    for (i = 0; i < 15; i++)
+    {
+        printf("\nDigite o %dº número: ", i + 1);
+        scanf("%d", &vetor[i]);
+    }
+    printf("Soma = %d", soma(vetor));
+}
