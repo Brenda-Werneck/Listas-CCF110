2f0008c90e0daa812e7b8032510926313a2940ac
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-13 08:04:54 -0300

    Update exercício 20.py

--- Lista 03/exercício 20.py
+++ Lista 03/exercício 20.py
@@ -1,7 +1,7 @@
 #Fazer um algoritmo que:
 #	Leia o valor de X;
 #	Calcule e escreva o valor do seguinte somatório:
-#(X^25)/1 + (X^24)/2 + (X^23)/3 + (X^22)/4 + ... + X/25
+#   (X^25)/1 + (X^24)/2 + (X^23)/3 + (X^22)/4 + ... + X/25
 
 X = int(input())
 cont = 1
