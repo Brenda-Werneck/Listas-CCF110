f2bd20dbbb0a1a255dc493d4954345f89207224b
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-13 08:05:36 -0300

    Update exercício 02.py

--- Lista 05/exercício 02.py
+++ Lista 05/exercício 02.py
@@ -1,4 +1,5 @@
 #A Caixa Econômica Federal (CEF) concederá um crédito especial com juros de 2% aos seus clientes de acordo com o saldo médio no último ano. Fazer um algoritmo que leia o saldo médio de um cliente e calcule o valor do crédito de acordo com a tabela a seguir. Escrever uma mensagem informando o saldo médio e o valor de crédito.
+
 #Saldo médio	        Percentual
 #De 0 a 500	        Nenhum crédito
 #De 501 a 1000	        30% do valor do saldo médio
