b11d73876f7e3b396a455e9388747b31cd69ce4e
Brenda-Werneck <89711195+Brenda-Werneck@users.noreply.github.com>
2021-10-14 09:09:01 -0300

    Update exercício 12.py

--- Lista 07/exercício 12.py
+++ Lista 07/exercício 12.py
@@ -1,11 +1,12 @@
 #Fazer um algoritmo que:
-#a.	Leia um conjunto de valores inteiros correspondentes a 80 notas dos alunos de uma turma, notas estas que variam de 0 a 10;
-#b.	Calcule a frequência absoluta e a frequência relativa de cada nota;
-#c.	Escreva uma tabela contendo os valores das notas (de 0 a 10) e suas respectivas frequências absoluta e relativa.
+#   a.	Leia um conjunto de valores inteiros correspondentes a 80 notas dos alunos de uma turma, notas estas que variam de 0 a 10;
+#   b.	Calcule a frequência absoluta e a frequência relativa de cada nota;
+#   c.	Escreva uma tabela contendo os valores das notas (de 0 a 10) e suas respectivas frequências absoluta e relativa.
+
 #Observações:
-#1.	Frequência absoluta de uma nota é o número de vezes em que aparece no conjunto de dados;
-#2.	Frequência relativa é a frequência absoluta dividida pelo número total de dados;
-#3.	Utilizar como vetor  somente as variáveis que forem necessárias.
+#   1.	Frequência absoluta de uma nota é o número de vezes em que aparece no conjunto de dados;
+#   2.	Frequência relativa é a frequência absoluta dividida pelo número total de dados;
+#   3.	Utilizar como vetor  somente as variáveis que forem necessárias.
 
 vetor = []
 
